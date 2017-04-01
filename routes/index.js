235fb4c106fbf3ab5c28c4ad5101bfe805972cb6
Swapnil Singh <swappp7singh@gmail.com>
2017-04-02 00:35:43 +0530

    project completed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.post('/', function (req, res) {
+    var api_key = 'key-yourkey';
+    var domain = 'yourdomain';
+    var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+    var data = {
+        from: 'AmazeSpace <admin@theamaze.space>',
+        to: req.body.to,
+        subject: req.body.subject,
+        text: req.body.message
+    };
+
+    mailgun.messages().send(data, function (error, body) {
+        if (!error){
+            res.send('Message has been sent');
+        } else {
+            console.log(error)
+        }
+    });
 });
 
 module.exports = router;
